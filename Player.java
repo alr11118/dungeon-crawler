7d61ea149eb459869a1cd4bd3852fef2d8bb62b3
Alara Engin <alara.engin2008@gmail.com>
2026-03-24 09:02:24 -0700

    checkpoint 3

--- Player.java
+++ Player.java
@@ -29,10 +29,16 @@ public class Player {
    public Item removeItemFromInventory(Item item) {
       // TODO: remove an item to inventory. Return the Item so it can returned to the Room that it is in
       inventory.remove(item);
+      return(item);
    }
    
    
    public boolean hasItem(String itemName) {
+      for(Item item : inventory) {
+         if(item.getItemType() == itemName) {
+            return true;
+         }
+      }
       return false;
    }
    
@@ -41,12 +47,24 @@ public class Player {
       // TODO: This method will take the player's current X and Y, and return the contents of the room
       // they are in with a descriptive String
       // HINT: Use map.getRoom(currentX, currentY) and call toString()
+      return map.getRoom(currentX, currentY).toString();
    }
    public void moveTo(int x, int y) {
       // TODO: Update the Player's currentX and currentY values.
+      this.currentX = x;
+      this.currentY = y;
    }
    
    public Room getRoom(Map map) {
       // TODO: return the room object that the player is currently in, given the Map that was passed
+      return map.getRoom(currentX, currentY);
+   }
+   
+   public static void main(String[] args) {
+      Player p = new Player(100, "Hero", 0, 0);
+      Map map = new Map(5, 6);
+      System.out.println(p.look(map));
+      p.moveTo(1, 2);
+      System.out.println(p.look(map));
    }
 }
